e59814130f094556401265f34fecb5fe3581729a
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-07-05 13:42:04 -0700

    updated Todo.js

--- app/components/Todo.js
+++ app/components/Todo.js
@@ -13,3 +13,17 @@ class Todo extends React.Component {
 }
 
 export default Todo;
+
+import React from 'react';
+
+const Todo = ({task, completed, handleOnClick}) => {
+  return (
+    <li>
+        <span onClick={handleOnClick}>
+          {completed ? <strike> {task} </strike> : task}
+        </span>
+    </li>
+  )
+}
+
+export default Todo;
