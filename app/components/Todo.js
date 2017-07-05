61d6cdb53f8ee6587a1e486f81e1550a38d6ee43
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-07-05 13:48:51 -0700

    updated TodoList.js

--- app/components/Todo.js
+++ app/components/Todo.js
@@ -1,21 +1,5 @@
 import React from 'react';
 
-class Todo extends React.Component {
-  render() {
-    return (
-      <li>
-        <span onClick={() => this.props.toggleTodo()}>
-          {this.props.completed ? <strike> {this.props.task}</strike> : this.props.task}
-        </span>
-      </li>
-    );
-  }
-}
-
-export default Todo;
-
-import React from 'react';
-
 const Todo = ({task, completed, handleOnClick}) => {
   return (
     <li>
