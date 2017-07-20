f4adfd468bff8df97c542012d128ed8e4d59778d
tlnguyen4 <tlnguyen@princeton.edu>
2017-07-20 15:05:37 -0700

    app working up to addTodo

--- app/components/TodoApp.js
+++ app/components/TodoApp.js
@@ -2,54 +2,40 @@ import React from 'react';
 import InputLine from './InputLine';
 import TodoList from './TodoList';
 
+import { connect } from 'react-redux';
+import { addTodo } from '../actions/index';
+
 
 let id = 0;
 
-class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { todos: [] };
-  }
+let TodoApp =({ todos, addTodoClick, toggleTodoClick }) => {
+  return (
+    <div>
+      <InputLine
+        addTodo={(task) => addTodoClick(id++, task)}
+      />
+      <TodoList
+        todos={todos}
+        handleToggleTodo={(id) => toggleTodoClick(id)}
+      />
+    </div>
+  )
+}
 
-  toggleTodo(index) {
-    const newTodo = Object.assign(
-      {},
-      this.state.todos[index],
-      { completed: !this.state.todos[index].completed }
-    );
-    const newTodos = [ 
-      ...this.state.todos.slice(0, index),
-      newTodo,
-      ...this.state.todos.slice(index + 1)  
-    ];
-    this.setState({ todos: newTodos });
-  }
-
-  addTodo(task) {
-    const newTodo = { 
-      id: id++,
-      task: task,
-      completed: false 
-    };
-    const newTodos = [ ...this.state.todos ]
-    newTodos.push(newTodo);
-    this.setState({ todos: newTodos });
-  }
-
-  render() {
-    return (
-      <div>
-        <InputLine
-          addTodo={(task) => this.addTodo(task)}
-        />
-        <TodoList
-          todos={this.state.todos}
-          toggleTodo={(index) => this.toggleTodo(index)}
-          removeTodo={(index) => this.removeTodo(index)}
-        />
-      </div>
-    );
+const mapStateToProps = state => {
+  return {
+    todos: state
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodoClick: (id, task) => {dispatch(addTodo(id, task))}
+  }
+}
+
+TodoApp = connect(
+  mapStateToProps, mapDispatchToProps
+)(TodoApp);
 
 export default TodoApp;
