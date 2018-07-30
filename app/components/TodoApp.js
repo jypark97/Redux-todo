122cb803bdfc65474d90772c9e57a3b946063dc0
Robert L <1998.robert@gmail.com>
2018-07-30 14:36:26 -0700

    first commit

--- app/components/TodoApp.js
+++ app/components/TodoApp.js
@@ -6,35 +6,35 @@ import TodoList from './TodoList';
 let id = 0;
 
 class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { todos: [] };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { todos: [] };
+  // }
 
-  toggleTodo(index) {
-    const newTodo = Object.assign(
-      {},
-      this.state.todos[index],
-      { completed: !this.state.todos[index].completed }
-    );
-    const newTodos = [ 
-      ...this.state.todos.slice(0, index),
-      newTodo,
-      ...this.state.todos.slice(index + 1)  
-    ];
-    this.setState({ todos: newTodos });
-  }
+  // toggleTodo(index) {
+  //   const newTodo = Object.assign(
+  //     {},
+  //     this.state.todos[index],
+  //     { completed: !this.state.todos[index].completed }
+  //   );
+  //   const newTodos = [
+  //     ...this.state.todos.slice(0, index),
+  //     newTodo,
+  //     ...this.state.todos.slice(index + 1)
+  //   ];
+  //   this.setState({ todos: newTodos });
+  // }
 
-  addTodo(task) {
-    const newTodo = { 
-      id: id++,
-      task: task,
-      completed: false 
-    };
-    const newTodos = [ ...this.state.todos ]
-    newTodos.push(newTodo);
-    this.setState({ todos: newTodos });
-  }
+  // addTodo(task) {
+  //   const newTodo = {
+  //     id: id++,
+  //     task: task,
+  //     completed: false
+  //   };
+  //   const newTodos = [ ...this.state.todos ]
+  //   newTodos.push(newTodo);
+  //   this.setState({ todos: newTodos });
+  // }
 
   render() {
     return (
@@ -44,8 +44,7 @@ class TodoApp extends React.Component {
         />
         <TodoList
           todos={this.state.todos}
-          toggleTodo={(index) => this.toggleTodo(index)}
-          removeTodo={(index) => this.removeTodo(index)}
+          handleToggleTodo={(id) => this.toggleTodo(id)}
         />
       </div>
     );
